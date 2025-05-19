07c07f80453734fe72eeeb02395e8a903897a65d
Sudipta <stark@Sudiptas-MacBook-Pro.local>
2025-05-19 17:11:40 +0530

    w

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         <div>
           <LoginComponent>
             <Button variant={"outline"} size={"lg"}>
-              Sign In
+              Sign in 
             </Button>
           </LoginComponent>
         </div>
